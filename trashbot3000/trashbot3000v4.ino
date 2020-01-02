aa92d5b38149d3fd138de7036d4297d6ae138e5e
imveryhighrightnow <59445285+imveryhighrightnow@users.noreply.github.com>
2020-01-02 09:06:53 -0600

    Arduino based project to open trash can with a proximity sensor.
    Arduino based project to open trash can with a proximity sensor.  Has led status lights and count down to close led.

---
+++ trashbot3000/trashbot3000v4.ino
@@ -0,0 +1,93 @@
+#include <Servo.h>
+// define the LED digit patterns, from 0 - 9
+// 1 = LED on, 0 = LED off, in this order:
+//                74HC595 pin     Q0,Q1,Q2,Q3,Q4,Q5,Q6,Q7 
+//                Mapping to      a,b,c,d,e,f,g of Seven-Segment LED
+byte seven_seg_digits[10] = { B11111100,  // = 0
+                              B01100000,  // = 1
+                              B11011010,  // = 2
+                              B11110010,  // = 3
+                              B01100110,  // = 4
+                              B10110110,  // = 5
+                              B10111110,  // = 6
+                              B11100000,  // = 7
+                              B11111110,  // = 8
+                              B11100110   // = 9
+                             };
+ 
+// connect to the ST_CP of 74HC595 (pin 3,latch pin)
+int latchPin = 3;
+// connect to the SH_CP of 74HC595 (pin 4, clock pin)
+int clockPin = 4;
+// connect to the DS of 74HC595 (pin 2)
+int dataPin = 2;
+Servo myservo;  
+int pos = 20;  
+const int trigPin = 6;
+const int echoPin = 5;
+const int led = 11;
+const int green = 12;
+const int red = 10;
+
+long duration;
+float distance;
+
+void setup() 
+{
+  myservo.attach(11);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT); 
+  pinMode(led, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  myservo.write(pos);
+}
+
+// display a number on the digital segment display
+void sevenSegWrite(byte digit) {
+  // set the latchPin to low potential, before sending data
+  digitalWrite(latchPin, LOW);
+     
+  // the original data (bit pattern)
+  shiftOut(dataPin, clockPin, LSBFIRST, seven_seg_digits[digit]);  
+ 
+  // set the latchPin to high potential, after sending data
+  digitalWrite(latchPin, HIGH);
+}
+void loop() 
+{
+  //Serial.begin(9600);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = 0.034*(duration/2);
+  //Serial.println(distance);
+  if (distance < 5)
+  {
+    digitalWrite(green, HIGH);
+    digitalWrite(red,LOW); 
+    
+    digitalWrite(led,HIGH);
+    myservo.write(pos+90);
+      for (byte digit = 10; digit > 0; --digit) {
+    delay(1000);
+    sevenSegWrite(digit - 1); 
+  }
+    delay(100);
+  }
+  else 
+  {
+    digitalWrite(led,LOW);
+    digitalWrite(red,HIGH);
+    digitalWrite(green,LOW);
+      myservo.write(pos);
+  }
+  delay(300);
+}
